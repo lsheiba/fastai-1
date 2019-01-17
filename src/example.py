b5d6bfad4782b1b7dc9a83605ba145f6afe196dc
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-01-17 16:14:16 +0400

    Fix script

--- src/example.py
+++ src/example.py
@@ -1,7 +1,9 @@
 from fastai import *
+from fastai.vision import *
 
 path = untar_data(URLs.MNIST_SAMPLE)
 
+print('Using path %s' % path)
 data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), bs=64)
 data.normalize(imagenet_stats)
 img, label = data.train_ds[0]

83e77bb787507f902229a894b7549dde0bb24496
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-01-18 13:09:09 +0400

    Try export model

--- src/example.py
+++ src/example.py
@@ -1,14 +1,39 @@
+import argparse
+from os import path
+
+import torch
 from fastai import *
 from fastai.vision import *
 
-path = untar_data(URLs.MNIST_SAMPLE)
 
-print('Using path %s' % path)
-data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), bs=64)
-data.normalize(imagenet_stats)
-img, label = data.train_ds[0]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train-dir',
+        required=True,
+        help='Directory containing the results of training'
+    )
+    return parser.parse_args()
 
-learn = create_cnn(data, models.resnet18, metrics=accuracy)
-learn.fit_one_cycle(1, 0.01)
 
-accuracy(*learn.get_preds())
+def main():
+    args = parse_args()
+    path = untar_data(URLs.MNIST_SAMPLE)
+
+    print('Using path %s' % path)
+    data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), bs=64)
+    data.normalize(imagenet_stats)
+
+    learn = create_cnn(data, models.resnet18, metrics=accuracy)
+    learn.fit_one_cycle(1, 0.01)
+
+    print(accuracy(*learn.get_preds()))
+
+    model = learn.model()
+    model_location = path.join(args.train_dir, "model.pth")
+    torch.save(model, model_location)
+
+    torch_model = torch.load(model_location)
+
+    print('Network structure:')
+    torch_model.eval()

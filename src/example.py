8dbd23e901bcd8526edd376d55ba6d32dc93ade5
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-01-18 13:13:01 +0400

    Fix path

--- src/example.py
+++ src/example.py
@@ -18,10 +18,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    path = untar_data(URLs.MNIST_SAMPLE)
+    data_path = untar_data(URLs.MNIST_SAMPLE, dest=args.train_dir)
 
-    print('Using path %s' % path)
-    data = ImageDataBunch.from_folder(path, ds_tfms=(rand_pad(2, 28), []), bs=64)
+    print('Using path %s' % data_path)
+    data = ImageDataBunch.from_folder(data_path, ds_tfms=(rand_pad(2, 28), []), bs=64)
     data.normalize(imagenet_stats)
 
     learn = create_cnn(data, models.resnet18, metrics=accuracy)
